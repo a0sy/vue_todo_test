93d966aa04d25e81954bb44af9ee9d5748c64473
a0sy <a0sy.gl@gmail.com>
2015-07-26 01:06:09 +0900

    0taskだとDoneを表示

--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,18 @@
 var app = new Vue({
   el: '#todo',
   data: {
-      todo:"",
-      todos: [],
-      font: ""
+      todo: "",
+      todos: []
   },
   computed: {
       lefts: function(){
           return this.todos.filter(function(t){ return !t.isDone }).length;
+      },
+      done: function() {
+        var t = "Done!";
+        if (this.lefts == 0) {
+          return t;
+        }
       }
   },
   methods:{
